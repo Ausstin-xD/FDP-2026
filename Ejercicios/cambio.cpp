0ea1cda7e20751d6f1fb6edc536d54c3785e181b
00155026@uca.edu.sv <00155026@uca.edu.sv>
2026-05-06 16:11:48 -0600

    cambio de dinero

--- Ejercicios/cambio.cpp
+++ Ejercicios/cambio.cpp
@@ -4,31 +4,34 @@ using namespace std;
 int main() {
 
 
-    int cambio, n, resultado;
+    int cambio, n, resultado, e, y, l;
 
 cout << "Hola, Prodias ingresar una opcion de cambio:" << endl;
 cout << "1. Dolar a Euro:" << endl;
 cout << "2. Dolar a Yen:" << endl; 
 cout << "3. Dolar a Libra:" << endl;
-
-
-   
-cout << "Ingrese la opcion de cambio: ";
+ 
+cout << "Ingrese la opcion de cambio: " << endl;
     cin >> cambio;
 
-cout << "Ingrese la cantidad en Dólares: ";
+cout << "Ingrese la cantidad en Dolares: " << endl;
     cin >> n;
 
 switch (cambio) {
 case 1:
-resultado = (n * 1);
-cout << "El resultado es: " << resultado << " Euros" << endl;
+e = (n * 1);
+cout << "El resultado es: " << e << " Euros" << endl;
+break;
 case 2:
-resultado = (n * 157);
-cout << "El resultado es: " << resultado << " Yenes" << endl;
+y = (n * 157);
+cout << "El resultado es: " << y << " Yenes" << endl;
+break;
 case 3:
-resultado = (n * 1);
-cout << "El resultado es: " << resultado << " Libras" << endl;
+l = (n * 1);
+cout << "El resultado es: " << l << " Libras" << endl;
+break;
+default:
+cout << "Ingresa una opcion valida (1-3)" << endl;
 }   
 
     return 0;

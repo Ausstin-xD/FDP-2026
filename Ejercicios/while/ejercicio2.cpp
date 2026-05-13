7612644c9862ac1579afefce5fd8b30726515977
00155026@uca.edu.sv <00155026@uca.edu.sv>
2026-05-12 22:26:08 -0600

    1

--- Ejercicios/while/ejercicio2.cpp
+++ Ejercicios/while/ejercicio2.cpp
@@ -1,17 +1,18 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
+    int num, suma = 0;
 
-int numero = 0;
+    do {
+        cout << "Ingresa un numero (0 para terminar): ";
+        cin >> num;
 
-cout << "Ingrese un numero: " << endl;
-    cin >> numero;
+        suma = suma + num;
 
-    while (numero == 0) {
-        cout << "El numero es diferente de cero: " << numero << endl;}
-    
+    } while (num != 0);
 
-        
+    cout << "La suma de los numeros ingresados es: " << suma << endl;
+
     return 0;
 }

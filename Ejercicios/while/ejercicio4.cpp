51c591c8e06d50eff1dfb01b6ce0446da55247ae
00155026@uca.edu.sv <00155026@uca.edu.sv>
2026-05-14 19:03:07 -0600

    Menu infinito

---
+++ Ejercicios/while/ejercicio4.cpp
@@ -0,0 +1,31 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+
+int menu,suma,num1,num2;
+do
+{
+    cout << "1. Sumar" << endl;
+    cout << "2. Salir" << endl;
+    cout << "Ingrese su opcion: ";
+    cin >> menu;
+
+    if (menu == 1)
+    {
+        cout << "Ingrese el primer numero: ";
+        cin >> num1;
+        cout << "Ingrese el segundo numero: ";
+        cin >> num2;
+        suma = num1 + num2;
+        cout << "La suma es: " << suma << endl;
+    }
+
+} while (menu != 2);
+
+
+
+
+
+    return 0;
+}

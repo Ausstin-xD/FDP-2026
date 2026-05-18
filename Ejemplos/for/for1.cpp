3a25b27cb730dfafc5a3214af192010fab93614f
00155026@uca.edu.sv <00155026@uca.edu.sv>
2026-05-18 11:26:32 -0600

    for

---
+++ Ejemplos/for/for1.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+
+    int n=0, a;
+    cout << "Podrias ingresar un numero que sera tu limite: " << endl;
+    cin >> a;
+    
+    for (int i = 1; i <= a; i++)
+    {
+        cout << i << " ";
+    }
+    
+cout << endl;
+        
+    return 0;
+}
